4860269e40f0c7ba7f6bddc50b1ee73202de115d
Arpit Abhyankar <arpitabhyankar99823@gmail.com>
2023-02-04 18:04:52 +0530

    last commit

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import Context,{ NewsContext } from './API/Context';
 
 
 function App() {
+
+  {/*
+  package.json newer dependencies:-
+  "@expo/vector-icons": "^13.0.0",
+    "@expo/webpack-config": "^0.17.2",
+    "axios": "^1.3.0",
+    "expo": "~42.0.0",
+    "expo-status-bar": "~1.4.2",
+    "react": "18.1.0",
+    "react-dom": "18.1.0",
+    "react-native": "https://github.com/expo/react-native/archive/sdk-42.0.0.tar.gz",
+    "react-native-pager-view": "6.0.1",
+    "react-native-snap-carousel": "^3.9.1",
+    "react-native-tab-view": "^3.3.4",
+    "react-native-web": "~0.18.7","*/}
+
   const {dark} = useContext(NewsContext);
   StatusBar.setBarStyle('dark-content', true);
   return (
